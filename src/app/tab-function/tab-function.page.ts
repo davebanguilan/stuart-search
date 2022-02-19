e609e3ce743677d576bb84736c38e5aab5d50e58
Dave Banguilan <57138219+davebanguilan@users.noreply.github.com>
2022-02-19 22:07:35 +0800

    Added android platform, resource, search (#4)
    Co-authored-by: Dave Emilson Banguilan <dbanguilan@you-source.com>

--- src/app/tab-function/tab-function.page.ts
+++ src/app/tab-function/tab-function.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Data, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ROUTEURL } from '../_shared/constants';
-import { PaListService } from '../_shared/services';
+import { Data } from '../_shared/models';
+import { PaListService, PlantService } from '../_shared/services';
 
 @Component({
   selector: 'app-tab-function',
@@ -10,21 +11,46 @@ import { PaListService } from '../_shared/services';
 })
 export class TabFunctionPage {
   choices: Data[];
+  selectedValue = '';
+  showErrorMessage = false;
+  errorMessage: string;
+  searchValue: string;
 
   constructor(
     private paListService: PaListService,
+    private plantService: PlantService,
     private router: Router
   ) {}
 
   ionViewWillEnter(): void {
-    this.choices = [];
     this.choices = this.paListService.paList;
   }
 
   activitySelected($event: any): void {
-    this.paListService.setPaTitle($event.target.value);
-    this.paListService.setPaSelected($event.target.value);
-    this.router.navigateByUrl(ROUTEURL.paList);
+    if(!!$event.target.value) {
+      this.paListService.setPaTitle($event.target.value);
+      this.paListService.setPaSelected($event.target.value);
+      this.router.navigateByUrl(ROUTEURL.paList);
+      this.selectedValue = null;
+    }
+  }
+
+  search(): void {
+    if(!!this.searchValue) {
+      this.plantService.setSelectedPlant(this.searchValue.toUpperCase());
+      this.router.navigateByUrl(ROUTEURL.plant);
+    } else {
+      this.errorMessage = 'Please provide the common name of the plant.';
+      this.showErrorMessage = true;
+    }
+    this.searchValue = null;
+  }
+
+  inputInFocus(): void {
+    if(this.showErrorMessage) {
+      this.errorMessage = '';
+      this.showErrorMessage = false;
+    }
   }
 
 }
